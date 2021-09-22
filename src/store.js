749d644b9e6853e30bb97a0eb0cfee1a5bd7ed53
alnwlkr <mendoza.allendg@gmail.com>
2021-09-22 16:53:30 +0800

    implement redux persist

--- src/store.js
+++ src/store.js
@@ -1,16 +1,24 @@
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from '@redux-saga/core';
 import { composeWithDevTools } from 'redux-devtools-extension';
-
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+ 
 import rootReducer from './reducers';
 import rootSaga from './sagas';
 
 const configureStore = () => {
     const sagaMiddleware = createSagaMiddleware();
+    const persistConfig = {
+        key: 'root',
+        storage,
+      };
+
+    const persistedReducer = persistReducer(persistConfig, rootReducer);
 
     return {
         ...createStore(
-            rootReducer,
+            persistedReducer,
             composeWithDevTools(applyMiddleware(sagaMiddleware)),
         ),
         runSaga: sagaMiddleware.run(rootSaga),
