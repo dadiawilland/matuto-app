e27d5f893fe3797f7814e5a62f7057507a2bb3ed
Willand <dadiawilland@gmail.com>
2021-09-29 14:29:56 +0800

    Updates

--- src/utils/utils.js
+++
@@ -1,7 +0,0 @@
-// import React, { useState, useCallback } from 'react'
-// import {useHistory} from 'react-router-dom';
-
-// export function navigateTo(location){
-//     const history = useHistory();
-//     return history.push('/pricing');
-// }
